00cc707274546a2b443428601b797a2dbec07d9e
ali <alilaser8080@gmail.com>
2023-12-28 20:57:53 +0330

    add version 3: word&char counter

--- main.py
+++ main.py
@@ -29,6 +29,12 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
+    if event == "Word Count":
+        full_text = values["-DATA-"]
+        clean_text = full_text.replace("\n", " ").split(" ")
+        word_count = len(clean_text)
+        char_count = len("".join(clean_text))
+        sg.popup_ok_cancel(f"Word count: {word_count}\nCharacter count: {char_count}", grab_anywhere=True)
     if event in smileys_menu:
         current_text = values["-DATA-"]
         new_text = current_text + " " + event
