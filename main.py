9f9ffed883bf93f82446710e4d89ed87a87adacb
ali <alilaser8080@gmail.com>
2023-12-28 20:52:24 +0330

    add version 2: smileys

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ layout_menu = [
 layout = [
     [sg.Menu(layout_menu)],
     [sg.Text("Untitled", key="-DOCNAME-")],
-    [sg.Multiline(key="-DATA-")],
+    [sg.Multiline(key="-DATA-", no_scrollbar=True, size=(60, 30))],
 ]
+
+smileys_menu = smileys[1] + smileys[3] + smileys[5]
 
 window = sg.Window("Text Editor", layout)
 
@@ -26,5 +28,10 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
+    if event in smileys_menu:
+        current_text = values["-DATA-"]
+        new_text = current_text + " " + event
+        window["-DATA-"].update(new_text)
 
 window.close()
